ce3e41b72d9ba463f991e1916b4cdd6556d43ef9
Mohammad Minhazul Abedin <84508269+minuwu@users.noreply.github.com>
2026-05-16 19:41:52 +0600

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(() => ({
   base: '/',
   plugins: [react()],
   build: {
